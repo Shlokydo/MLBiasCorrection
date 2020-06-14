3dafb05e6c7e1f1d9a6ecb876cf54e069505f42e
aamemiya <arata.amemiya@riken.jp>
2020-06-14 22:55:47 +0900

    calculation of average RMSE of extended forecasts

--- forecast.py
+++ forecast.py
@@ -37,8 +37,11 @@ bc_gamma = param.param_bc['gamma']
 
 intv_nature=int(dt_nature/dt)
 
-inittime=int(sys.argv[1])
+#inittime=int(sys.argv[1])
 length=200
+nsmpmax=20
+intv=250
+inittime=4500+250   ### spinup
 
 #------------------------------------------------
 
@@ -54,86 +57,95 @@ nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/assim.nc','r',forma
 analysis = np.array(nc.variables['va'][:], dtype=type(np.float64)).astype(np.float32)
 time_assim = np.array(nc.variables['t'][:], dtype=type(np.float64)).astype(np.float32)
 nc.close
-for i in range(nmem):
-  letkf.ensemble[i].x = analysis[inittime,i,:]
 
-inittime_nature = int(np.where(time_nature == time_assim[inittime])[0])
-ntime_nature=len(time_nature)
+assim_length=len(time_assim)
 
-bc=BC.BiasCorrection(bc_type,nx,bc_alpha,bc_gamma)
-
-xf=[]
-xfm=[]
-xfm_raw=[]
-rmse=[]
-sprd=[]
-# MAIN LOOP
-#for step in range(inittime_nature, ntime_nature):
-for step in range(inittime_nature, inittime_nature+length):
-  for i in range(intv_nature):
-    letkf.forward()
+ismp=0
+rmsesmp=[]
+sprdsmp=[]
+while ((inittime+length <= assim_length) and (ismp < nsmpmax)) :
   
-  xfmtemp=letkf.mean()
-  xfm_raw.append(xfmtemp)
+  print("inittime ",inittime)
+  
   for i in range(nmem):
-    letkf.ensemble[i].x = bc.correct(letkf.ensemble[i].x)
-  xf.append(letkf.members())
-  xfmtemp=letkf.mean()
-  xfm.append(xfmtemp)
+    letkf.ensemble[i].x = analysis[inittime,i,:]
 
-  rmse.append(math.sqrt(((letkf.mean()-nature[step+1])**2).sum() /nx))
+  inittime_nature = int(np.where(time_nature == time_assim[inittime])[0])
+  ntime_nature=len(time_nature)
+
+  bc=BC.BiasCorrection(bc_type,nx,bc_alpha,bc_gamma)
+
+  xf=[]
+  xfm=[]
+  xfm_raw=[]
+  rmse=[]
+  sprd=[]
+
+# init value
+  xf.append(letkf.members())
+  xfm.append(letkf.mean())
+  xfm_raw.append(letkf.mean())
+  rmse.append(math.sqrt(((letkf.mean()-nature[inittime_nature])**2).sum() /nx))
   sprd.append(math.sqrt((letkf.sprd()**2).sum()/nx))
+# 
+
+# MAIN LOOP
+  for step in range(inittime_nature, inittime_nature+length):
+    for i in range(intv_nature):
+      letkf.forward()
+      if param.param_bc['correct_step'] is not None:
+        for i in range(nmem):
+          fact=1.0/intv_assim
+          letkf.ensemble[i].x = fact * bc.correct(letkf.ensemble[i].x) + (1.0-fact) * letkf.ensemble[i].x
+   
+    xfmtemp=letkf.mean()
+    xfm_raw.append(xfmtemp)
+    if param.param_bc['correct_step'] is not None:
+       for i in range(nmem):
+            fact=1.0/intv_assim
+            letkf.ensemble[i].x = fact * bc.correct(letkf.ensemble[i].x) + (1.0-fact) * letkf.ensemble[i].x
+    else:
+       for i in range(nmem):
+            letkf.ensemble[i].x = bc.correct(letkf.ensemble[i].x)
+ 
+    xf.append(letkf.members())
+    xfmtemp=letkf.mean()
+    xfm.append(xfmtemp)
+
+    rmse.append(math.sqrt(((letkf.mean()-nature[step+1])**2).sum() /nx))
+    sprd.append(math.sqrt((letkf.sprd()**2).sum()/nx))
 #  if ( round(icount/10,4).is_integer() ):
 #  print("time {0:10.4f}, RMSE , {1:8.4f}, SPRD ,{2:8.4f}".format(round(time_nature[step],4),rmse[step-inittime_nature],sprd[step-inittime_nature]))
         
 #print("done")
 
+  rmsesmp.append(rmse)
+  sprdsmp.append(sprd)
 
-xf  = np.array(xf,  dtype=np.float64)
-xfm_raw = np.array(xfm_raw, dtype=np.float64)
-xfm = np.array(xfm, dtype=np.float64)
-rmse  = np.array(rmse,  dtype=np.float64)
-sprd  = np.array(sprd,  dtype=np.float64)
+  ismp+=1 
+  inittime+=intv
 
-os.system('mkdir -p ' + expdir + '/' + obsdir + '/' + dadir)
- 
-nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/forecast.nc','w',format='NETCDF3_CLASSIC')
-nc.createDimension('x',nx)
-nc.createDimension('e',nmem)
-nc.createDimension('t',length)
-x_in = nc.createVariable('x',np.dtype('float64').char,('x'))
-e_in = nc.createVariable('e',np.dtype('float64').char,('e'))
-t_in = nc.createVariable('t',np.dtype('float64').char,('t'))
-v_in = nc.createVariable('v',np.dtype('float64').char,('t','x'))
-vf_in = nc.createVariable('vf',np.dtype('float64').char,('t','e','x'))
-vfm_in = nc.createVariable('vfm',np.dtype('float64').char,('t','x'))
-vfm_raw_in = nc.createVariable('vfm_raw',np.dtype('float64').char,('t','x'))
-x_in[:] = np.array(range(1,1+nx))
-e_in[:] = np.array(range(1,1+nmem))
-t_in[:] = np.round(time_nature[:length],4)
-v_in[:,:] = nature[:length]
-vf_in[:,:,:] = xf
-vfm_in[:,:] = xfm
-vfm_raw_in[:,:] = xfm_raw
-nc.close 
+rmsesmp  = np.array(rmsesmp,  dtype=np.float64)
+sprdsmp  = np.array(sprdsmp,  dtype=np.float64)
 
 if (os.path.isfile(expdir + '/' + obsdir + '/' + dadir + '/stats.nc')) :
-  nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/stats.nc','r+',format='NETCDF3_CLASSIC')
-  nsmp=len(nc.variables['rmse'])
-  nc.variables['rmse'][nsmp,:]=rmse
-  nc.variables['sprd'][nsmp,:]=sprd
-  nc.close
+    print('overwrite')
+    nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/stats.nc','r+',format='NETCDF3_CLASSIC')
+    nsmp=len(nc.variables['rmse'])
+    nc.variables['rmse'][nsmp:,:]=rmsesmp
+    nc.variables['sprd'][nsmp:,:]=sprdsmp
+    nc.close
 else : 
-  nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/stats.nc','w',format='NETCDF3_CLASSIC')
-  nc.createDimension('t',length)
-  nc.createDimension('s',None)
-  rmse_in = nc.createVariable('rmse',np.dtype('float64').char,('s','t'))
-  sprd_in = nc.createVariable('sprd',np.dtype('float64').char,('s','t'))
-  t_in = nc.createVariable('t',np.dtype('float64').char,('t'))
-  rmse_in[0,:] = rmse
-  sprd_in[0,:] = sprd
-  t_in[:] = np.round(time_nature[:length],4)
-  nc.close
-
+    print('create')
+    nc = netCDF4.Dataset(expdir + '/' + obsdir + '/' + dadir + '/stats.nc','w',format='NETCDF3_CLASSIC')
+    nc.createDimension('t',length+1)
+    nc.createDimension('s',None)
+    rmse_in = nc.createVariable('rmse',np.dtype('float64').char,('s','t'))
+    sprd_in = nc.createVariable('sprd',np.dtype('float64').char,('s','t'))
+    t_in = nc.createVariable('t',np.dtype('float64').char,('t'))
+    rmse_in[:,:] = rmsesmp
+    sprd_in[:,:] = sprdsmp
+    t_in[:] = np.round(time_nature[:length+1],4)
+    nc.close
 
 
--- plot_rmse.py
+++ plot_rmse.py
@@ -13,49 +13,50 @@ expdir = param.param_exp['expdir']
 obsdir = param.param_exp['obs_type']
 #dadir  = param.param_exp['da_type']
 
-dadir=('nocorr','offline_linear')
+dadir=('nocorr','linear_offline','test_tf')
 
 #------------------------------------------------
 
-# load nature and observation data
-nc = netCDF4.Dataset(expdir + '/' + obsdir +'/' + dadir[0] + '/stats.nc','r',format='NETCDF4')
-rmse = np.array(nc.variables['rmse'][:], dtype=type(np.float64)).astype(np.float32)
-sprd = np.array(nc.variables['sprd'][:], dtype=type(np.float64)).astype(np.float32)
-time = np.array(nc.variables['t'][:], dtype=type(np.float64)).astype(np.float32)
-nc.close 
+nexp=len(dadir)
 
-nc = netCDF4.Dataset(expdir + '/' + obsdir +'/' + dadir[1] + '/stats.nc','r',format='NETCDF4')
-rmse_2 = np.array(nc.variables['rmse'][:], dtype=type(np.float64)).astype(np.float32)
-sprd_2 = np.array(nc.variables['sprd'][:], dtype=type(np.float64)).astype(np.float32)
-time = np.array(nc.variables['t'][:], dtype=type(np.float64)).astype(np.float32)
-nc.close 
+rmse=[]
+sprd=[]
+rmse_plot=[]
+sprd_plot=[]
+
+
+# load nature and observation data
+for i in range(nexp):
+  nc = netCDF4.Dataset(expdir + '/' + obsdir +'/' + dadir[i] + '/stats.nc','r',format='NETCDF4')
+  rmse.append(np.array(nc.variables['rmse'][:], dtype=type(np.float64)).astype(np.float32))
+  sprd.append(np.array(nc.variables['sprd'][:], dtype=type(np.float64)).astype(np.float32))
+  if (i == 0): 
+    time = np.array(nc.variables['t'][:], dtype=type(np.float64)).astype(np.float32)
+  nc.close 
+  rmse_plot.append(np.mean(rmse[i],axis=0))
+  sprd_plot.append(np.mean(sprd[i],axis=0))
+
 
 ntime=len(time)
 
-
-rmse_plot=np.mean(rmse,axis=0)
-sprd_plot=np.mean(sprd,axis=0)
-
-rmse_2_plot=np.mean(rmse_2,axis=0)
-sprd_2_plot=np.mean(sprd_2,axis=0)
-
-
 doubling_time=0.2*2.1 ### 2.1 day
 
-refy=np.array((1,max(rmse_plot)))
-refx=np.array((0,doubling_time*np.log2(refy[1])))
+#refy=np.array((1,max(rmse_plot[0])))
+#refx=np.array((0,doubling_time*np.log2(refy[1])))
 
 smp_e=1
-nplt=200
+nplt=40
 ###
 plt.figure()
 plt.yscale('log')
 #plt.scatter(test_labels, test_predictions)
 #plt.scatter(fcst[ntime-100:ntime,1], anal[ntime-100:ntime,1]-fcst[ntime-100:ntime,1])
-plt.plot(time, rmse_plot)
-plt.plot(time, rmse_2_plot)
-#plt.plot(time, sprd_plot)
-plt.plot(refx, refy,color='black',linestyle='dashed')
+for i in range(nexp):
+  plt.plot(time[:nplt], rmse_plot[i][:nplt],label=dadir[i])
+# plt.plot(time, sprd_plot)
+
+plt.legend(bbox_to_anchor=(0.99,0.01), loc='lower right', borderaxespad=0,fontsize=14)
+#plt.plot(refx, refy,color='black',linestyle='dashed')
 plt.xlabel('time')
 plt.ylabel('RMSE')
 #plt.axis('equal')
